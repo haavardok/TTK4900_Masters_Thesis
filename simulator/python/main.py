20d7e78909f0095df080b9b127ec9e558a1cd0ce
haavardok <52386166+haavardok@users.noreply.github.com>
2023-11-20 16:49:33 +0100

    Code runs but NLP does not converge

--- simulator/python/main.py
+++ simulator/python/main.py
@@ -75,9 +75,9 @@ lx1 = -35; ly1 = 7                                  # azimuth thruster 1 positio
 lx2 = -35; ly2 = -7                                 # azimuth thruster 2 position (m)
 lx3 = 35;  ly3 = 0                                  # bow tunnel thruster position (m)
 alpha3 = math.pi/2                                  # bow tunnel thruster angle (rad)
-f1_min = -1/30 * m; f1_max = 1/30 * m               # azimuth thruster 1 force saturation (kN) -------------------------------CHECK THIS-------------------------------------------
-f2_min = -1/30 * m; f2_max = 1/30 * m               # azimuth thruster 2 force saturation (kN) -------------------------------CHECK THIS-------------------------------------------
-f3_min = -1/60 * m; f3_max = 1/60 * m               # bow tunnel thruster force saturation (kN) ------------------------------CHECK THIS-------------------------------------------
+f1_min = -1/30 * m*g; f1_max = 1/30 * m*g               # azimuth thruster 1 force saturation (kN) -------------------------------CHECK THIS-------------------------------------------
+f2_min = -1/30 * m*g; f2_max = 1/30 * m*g               # azimuth thruster 2 force saturation (kN) -------------------------------CHECK THIS-------------------------------------------
+f3_min = -1/60 * m*g; f3_max = 1/60 * m*g               # bow tunnel thruster force saturation (kN) ------------------------------CHECK THIS-------------------------------------------
 alpha1_min = -170*math.pi/180; alpha1_max = 170*math.pi/180                 # azimuth thruster 1 angle saturation (deg)
 alpha2_min = -170*math.pi/180; alpha2_max = 170*math.pi/180                 # azimuth thruster 1 angle saturation (deg)
 alpha3 = math.pi/2                                  # bow tunnel thruster constant angle (rad)
@@ -105,8 +105,9 @@ D_bis = np.array([                                  # non-dimensional dampening 
 M_vessel = m * N_mtrx @ M_bis @ N_mtrx                     # vessel mass matrix (kg)
 D_vessel = m * math.sqrt(g/L) * N_mtrx @ D_bis @ N_mtrx    # vessel dampening matrix (kg)
 
-f_min = np.array([f1_min, f2_min, f3_min])
-f_max = np.array([f1_max, f2_max, f3_max])
+
+# f_min = np.array([f1_min, f2_min, f3_min])
+# f_max = np.array([f1_max, f2_max, f3_max])
 
 # alpha_min_values = np.array([alpha1_min, alpha2_min])
 # alpha_max_values = np.array([alpha1_max, alpha2_max])
@@ -169,10 +170,10 @@ y   = ca.SX.sym('y')
 psi = ca.SX.sym('psi')
 eta = ca.vertcat(x,y,psi)
 
-x_d   = ca.SX.sym('x_d')
-y_d   = ca.SX.sym('y_d')
-psi_d = ca.SX.sym('psi_d')
-eta_d = ca.vertcat(x_d,y_d,psi_d)
+# x_d   = ca.SX.sym('x_d')                      # -----------------------HOW TO HANDLE ETA_D?-----------------------
+# y_d   = ca.SX.sym('y_d')
+# psi_d = ca.SX.sym('psi_d')
+# eta_d = ca.vertcat(x_d,y_d,psi_d)
 
 u  = ca.SX.sym('u')
 v  = ca.SX.sym('v')
@@ -188,11 +189,13 @@ alpha1 = ca.SX.sym('alpha1')
 alpha2 = ca.SX.sym('alpha2')
 alpha  = ca.vertcat(alpha1,alpha2)
 
-X = ca.vertcat(eta,eta_d,nu)        # NLP state vector -------------MAYBE ETA_D INTO U INSTEAD??-------------
-U = ca.vertcat(f_thr,alpha)         # NLP input vector
+X = ca.vertcat(eta,nu)                  # NLP state vector
+U = ca.vertcat(f_thr,alpha)             # NLP input vector
 
-x_init = [150,-325,-math.pi, 0,0,0, 0,0,0]
+x_init = [150,-325,-math.pi, 0,0,0]     # x(0)=150, y(0)=-325, psi(0)=-pi, u(0)=0, v(0)=0, r(0)=0
 u_init = [0,0,0, 0,0]
+u_min = [f1_min,f2_min,f3_min, alpha1_min,alpha2_min]
+u_max = [f1_max,f2_max,f3_max, alpha1_max,alpha2_max]
 
 # Model equations
 eta_dot = ca.mtimes(J(psi), nu)
@@ -200,7 +203,8 @@ nu_dot = ca.mtimes(ca.inv(M_vessel), ca.mtimes(T(alpha), f_thr) - ca.mtimes(D_ve
 
 # Objective term
 obj_det = ca.det(ca.mtimes(T(alpha), ca.mtimes(ca.inv(W), T(alpha).T)))     # singular configuration cost
-objective = (ca.mtimes((eta-eta_d).T, ca.mtimes(Q_eta, (eta-eta_d))) +      # continuous time objective
+objective = (ca.mtimes((eta).T, ca.mtimes(Q_eta, (eta))) +                  # ----------------------------HOW TO HANDLE ETA_D---------------------
+# objective = (ca.mtimes((eta-eta_d).T, ca.mtimes(Q_eta, (eta-eta_d))) +      # continuous time objective
              ca.mtimes(nu.T, ca.mtimes(Q_nu, nu)) +
              ca.mtimes(f_thr.T, ca.mtimes(R_f, f_thr)) +
              rho / (epsilon + obj_det))
@@ -231,7 +235,7 @@ Xk = ca.MX.sym('X0', X.size1())
 w.append(Xk)
 lbw.append(x_init)      # Equality constraint so bounded upper and lower --------------------------CHECK THIS---------------------------------
 ubw.append(x_init)
-w0.append(x_init)       # x(0)=150, y(0)=-325, psi(0)=-pi, x_d(0)=150, y_d(0)=-325, psi_d(0)=-pi, u(0)=0, v(0)=0, r(0)=0
+w0.append(x_init)       # x(0)=150, y(0)=-325, psi(0)=-pi, u(0)=0, v(0)=0, r(0)=0
 x_plot.append(Xk)
 
 # Formulate the NLP
@@ -239,8 +243,148 @@ for k in range(N):
     # New NLP variable for the control
     Uk = ca.MX.sym('U_' + str(k), U.size1())
     w.append(Uk)
-    lbw.append([-1])
-    ubw.append([1])
-    w0.append([0])
+    lbw.append(u_min)
+    ubw.append(u_max)
+    w0.append(u_init)       # "Initial guess" for the input at each opt. point
     u_plot.append(Uk)
 
+    # State at collocation points
+    Xc = []
+    for j in range(d):
+        Xkj = ca.MX.sym('X_'+str(k)+'_'+str(j), X.size1())
+        Xc.append(Xkj)
+        w.append(Xkj)                                                       # Optimize at each collocation point
+        lbw.append([-np.inf,-np.inf,-np.inf, -np.inf,-np.inf,-np.inf])      # Vessel states are unbounded (for now having no spatial constr.)
+        ubw.append([np.inf,np.inf,np.inf, np.inf,np.inf,np.inf])
+        w0.append([0,0,0,0,0,0])                                                   # "Initial guess" for the states at each opt. point
+
+    # Loop over collocation points
+    Xk_end = D[0]*Xk                        # Link the states to the next opt. prob.
+    for j in range(1,d+1):
+        # Expression for the state derivative at the collocation point
+        xp = C[0,j]*Xk
+        for r in range(d): xp = xp + C[r+1,j]*Xc[r]
+
+        # Append collocation equations
+        fj1, fj2, qj = f(Xc[j-1],Uk)
+        fj = ca.vertcat(fj1,fj2)            # Need to concatenate eta_dot and nu_dot into xdot -------------------------THIS MAY BE INCORRECT-------------------------------
+        g.append(h*fj - xp)                 # See Gros 2022: Step length times x_dot - x_p
+        lbg.append([0,0,0, 0,0,0])          # -------------------------ARE THESE CORRECT?-------------------------------
+        ubg.append([0,0,0, 0,0,0])
+
+        # Add contribution to the end state
+        Xk_end = Xk_end + D[j]*Xc[j-1]
+
+        # Add contribution to quadrature function
+        J = J + B[j]*qj*h
+
+    # New NLP variable for state at end of interval
+    Xk = ca.MX.sym('X_' + str(k+1), X.size1())
+    w.append(Xk)
+    lbw.append([-np.inf,-np.inf,-np.inf, -np.inf,-np.inf,-np.inf])
+    ubw.append([np.inf,np.inf,np.inf, np.inf,np.inf,np.inf])
+    w0.append([0,0,0,0,0,0])                       # -------------------------THIS MAY BE INCORRECT-------------------------------
+    x_plot.append(Xk)
+
+    # Add equality constraint
+    g.append(Xk_end-Xk)
+    lbg.append([0,0,0, 0,0,0])              # -------------------------THIS MAY BE INCORRECT-------------------------------
+    ubg.append([0,0,0, 0,0,0])
+
+# Concatenate vectors
+w = ca.vertcat(*w)
+g = ca.vertcat(*g)
+x_plot = ca.horzcat(*x_plot)
+u_plot = ca.horzcat(*u_plot)
+w0 = np.concatenate(w0)
+lbw = np.concatenate(lbw)
+ubw = np.concatenate(ubw)
+lbg = np.concatenate(lbg)
+ubg = np.concatenate(ubg)
+
+# Create an NLP solver
+prob = {'f': J, 'x': w, 'g': g}
+solver = ca.nlpsol('solver', 'ipopt', prob)
+
+# Function to get x and u trajectories from w
+trajectories = ca.Function('trajectories', [w], [x_plot, u_plot], ['w'], ['x', 'u'])
+
+# Solve the NLP
+sol = solver(x0=w0, lbx=lbw, ubx=ubw, lbg=lbg, ubg=ubg)
+x_opt, u_opt = trajectories(sol['x'])
+x_opt = x_opt.full() # to numpy array
+u_opt = u_opt.full() # to numpy array
+
+# Plot the result
+tgrid = np.linspace(0, time_horizon, N+1)
+tgrid2 = np.linspace(0, time_horizon, N)
+
+fig1, subplot1 = plt.subplots(3, sharex=True)
+subplot1[0].plot(tgrid, x_opt[0], '-', label="$x$ [m]")
+subplot1[0].legend(loc='upper right')
+# subplot1[0].set_ylim([0,200])
+subplot1[0].grid()
+subplot1[1].plot(tgrid, x_opt[1], '-', label="$y$ [m]")
+subplot1[1].legend(loc='upper right')
+# subplot1[1].set_ylim([-350,0])
+subplot1[1].grid()
+subplot1[2].plot(tgrid, x_opt[2], '-', label="$\\psi$ [rad]")
+subplot1[2].legend(loc='upper right')
+# subplot1[2].set_ylim([-3.5,0])
+subplot1[2].grid()
+fig1.suptitle("Position and angle vector $\\boldsymbol{\\eta}$")
+fig1.supxlabel("t")
+
+fig2, subplot2 = plt.subplots(3, sharex=True)
+subplot2[0].plot(tgrid, x_opt[3], '-', label="$u$ [m/s]")
+subplot2[0].legend(loc='upper right')
+subplot2[0].grid()
+subplot2[1].plot(tgrid, x_opt[4], '-', label="$v$ [m/s]")
+subplot2[1].legend(loc='upper right')
+subplot2[1].grid()
+subplot2[2].plot(tgrid, x_opt[5], '-',label="$r$ [rad/s]")
+subplot2[2].legend(loc='upper right')
+subplot2[2].grid()
+fig2.suptitle("Linear and angular velocity vector $\\boldsymbol{\\nu}$")
+fig2.supxlabel("t")
+
+fig3, subplot3 = plt.subplots(3, sharex=True)
+subplot3[0].plot(tgrid2, u_opt[0], '-', label="$f_1$ [kN]")
+subplot3[0].legend(loc='upper right')
+subplot3[0].grid()
+subplot3[1].plot(tgrid2, u_opt[1], '-', label="$f_2$ [kN]")
+subplot3[1].legend(loc='upper right')
+subplot3[1].grid()
+subplot3[2].plot(tgrid2, u_opt[2], '-', label="$f_3$ [kN]")
+subplot3[2].legend(loc='upper right')
+subplot3[2].grid()
+fig3.suptitle("Thruster forces $\\boldsymbol{f}$")
+fig3.supxlabel("t")
+
+fig4, subplot4 = plt.subplots(2, sharex=True)
+subplot4[0].plot(tgrid2, u_opt[3], '-', label="$\\alpha_1$ [rad]")
+subplot4[0].legend(loc='upper right')
+subplot4[0].grid()
+subplot4[1].plot(tgrid2, u_opt[4], '-', label="$\\alpha_2$ [rad]")
+subplot4[1].legend(loc='upper right')
+subplot4[1].grid()
+fig4.suptitle("Thruster angles $\\boldsymbol{\\alpha}$")
+fig4.supxlabel("t")
+
+plt.show()
+
+# plt.plot(tgrid, x_opt[0], '--')
+# plt.plot(tgrid, x_opt[1], '-')
+# plt.plot(tgrid, x_opt[2], '-')
+# plt.xlabel('t')
+# plt.legend(['x','y','psi'])
+# plt.grid()
+
+# plt.figure(2)
+# plt.step(tgrid, np.append(np.nan, u_opt[0]), '-.')
+# plt.step(tgrid, np.append(np.nan, u_opt[1]), '-.')
+# plt.step(tgrid, np.append(np.nan, u_opt[2]), '-.')
+# plt.xlabel('t')
+# plt.legend(['f1','f2','f3'])
+# plt.grid()
+# plt.show()
